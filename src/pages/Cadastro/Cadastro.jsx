2d04735ed515289c11d46f03623bed340fc94f8a
Claudiondev <claudinhon152@gmail.com>
2026-03-13 21:13:48 -0300

    feat: adiciona páginas de Login e Cadastro

---
+++ src/pages/Cadastro/Cadastro.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+import axios from "axios";
+
+import { Link } from "react-router-dom";
+
+
+
+function Cadastro() {
+    const [usuario, setUsuario] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const [repetirSenha, setRepetirSenha] = useState("");
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      if (senha !== repetirSenha) {
+        console.error('As senhas não coincidem');
+        return; 
+      }
+      axios.post('http://localhost:8080/auth/registrar', { email, senha, usuario, repetirSenha })
+        .then(response => {
+          console.log('Cadastro bem-sucedido:', response.data);
+          // Aqui você pode redirecionar o usuário ou armazenar o token de autenticação
+        })
+       .catch(error => {
+        console.error('Erro ao fazer cadastro:', error);
+        // Aqui você pode exibir uma mensagem de erro para o usuário
+      }) };
+
+  return (
+    <div>
+      <h1>Cadastro</h1>
+        <form>
+          <div>
+            <label htmlFor="usuario">Nome:</label>
+            <input type="text" id="usuario" value={usuario} onChange={(e) => setUsuario(e.target.value)} />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          <div>
+            <label htmlFor="senha">Senha:</label>
+            <input type="password" id="senha" value={senha} onChange={(e) => setSenha(e.target.value)} />
+          </div>
+            <div>
+            <label htmlFor="repetirSenha">Repita a Senha:</label>
+            <input type="password" id="repetirSenha" value={repetirSenha} onChange={(e) => setRepetirSenha(e.target.value)} />
+          </div>
+          <button type="submit" onClick={handleSubmit}>
+            Cadastrar
+          </button>
+        </form>
+        <Link to="/login">Já tem uma conta? Faça login</Link>  
+    </div>
+  );
+}
+
+export default Cadastro;
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,49 @@
+import axios from 'axios'; 
+
+import { useState } from 'react';
+
+import { Link } from 'react-router-dom'; 
+
+
+
+function Login() {
+
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const handleSubmit = (e) => {
+
+    e.preventDefault();
+    axios.post('http://localhost:8080/auth/login', { email, senha })
+      .then(response => {
+        console.log('Login bem-sucedido:', response.data);
+        // Aqui você pode redirecionar o usuário ou armazenar o token de autenticação
+      })
+      .catch(error => {
+        console.error('Erro ao fazer login:', error);
+        // Aqui você pode exibir uma mensagem de erro para o usuário
+      });
+  };
+
+  return (
+    <div>
+      <h1>Entrar</h1>
+      <form>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="senha">Senha:</label>
+          <input type="password" id="senha" value={senha} onChange={(e) => setSenha(e.target.value)} />
+        </div>
+        <button type="submit" onClick={handleSubmit}>
+          Entrar
+        </button>
+      </form>
+        <Link to="/cadastro">Não tem uma conta? Cadastre-se</Link>
+    </div>
+  );
+}
+
+export default Login;
