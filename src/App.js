5fc97674d6d52f4beae7bd143966b5b2c0e2764b
claudiondev <claudinhon152@gmail.com>
2026-03-25 13:56:27 -0300

    Fix: Corrigindo caminho do App.js e rotas

--- src/App.js
+++ src/App.js
@@ -7,19 +7,26 @@ import Salario from "./pages/Salarios/Salarios";
 import Resumo from "./pages/Resumo/Resumo";
 
 
-
 function App() {
   const token = localStorage.getItem('token');
+  
   return (
     <BrowserRouter>
       <Routes>
+        {/* ROTA PRINCIPAL: Abre o Login logo de cara */}
+        <Route path="/" element={<Login />} />
+
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro/>} />
         <Route path="/gastos" element={<Gastos />} />
         <Route path="/salarios" element={<Salario/>} />
         <Route path="/resumo" element={<Resumo/>}  />
+
+        {/* ROTA DE ERRO: Se o usuário digitar qualquer coisa errada, volta pro Login */}
+        <Route path="*" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
 export default App;
