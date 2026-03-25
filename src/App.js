e32ca0917fd4a5d4dbd1a776be3c954c48a9cb41
claudiondev <claudinhon152@gmail.com>
2026-03-25 14:30:28 -0300

    Fix: Forçando rota do Login com asterisco

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Login from "./pages/Login/Login";  
-import Cadastro from "./pages/Cadastro/Cadastro";
-import Gastos from "./pages/Gastos/Gastos";
-import Salario from "./pages/Salarios/Salarios";
-import Resumo from "./pages/Resumo/Resumo";
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login/Login'; // <--- Confira se o 'L' é maiúsculo na pasta também!
+import Cadastro from './pages/Cadastro/Cadastro';
+import Salarios from './pages/Salarios/Salarios';
+import Gastos from './pages/Gastos/Gastos';
+import Resumo from './pages/Resumo/Resumo';
 
 function App() {
-  const token = localStorage.getItem('token');
-  
   return (
     <BrowserRouter>
       <Routes>
-        {/* ROTA PRINCIPAL: Abre o Login logo de cara */}
+        {/* Usando o "*" garantimos que qualquer erro de rota caia no Login */}
         <Route path="/" element={<Login />} />
-
-        <Route path="/login" element={<Login />} />
-        <Route path="/cadastro" element={<Cadastro/>} />
+        <Route path="*" element={<Login />} /> 
+        
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/salarios" element={<Salarios />} />
         <Route path="/gastos" element={<Gastos />} />
-        <Route path="/salarios" element={<Salario/>} />
-        <Route path="/resumo" element={<Resumo/>}  />
-
-        {/* ROTA DE ERRO: Se o usuário digitar qualquer coisa errada, volta pro Login */}
-        <Route path="*" element={<Login />} />
+        <Route path="/resumo" element={<Resumo />} />
       </Routes>
     </BrowserRouter>
   );
